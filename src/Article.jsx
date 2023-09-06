d2a54c20250f22c17410b9e2329b55eed0394d25
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-09-06 14:06:21 +0900

    Update_Hooksでstateを扱う

--- src/Article.jsx
+++ src/Article.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import LikeButton from './LikeButton';
+import React, { useState } from 'react';
+// import LikeButton from './LikeButton';
 
 const Article = (props) => {
+  // isPublishedの初期状態はfalse、isPublishedを変更する関数としてtogglePublishedを宣言
+  const [isPublished, togglePublished] = useState(false);
   return (
     <div>
       <h2>{props.title}</h2>
       <label htmlFor="check">公開状態：</label>
       <input
         type="checkbox"
-        checked={props.ispublished}
+        checked={isPublished}
         id="check"
-        onClick={() => props.toggle()}
+        onClick={() => togglePublished(!isPublished)}
       />
-      <LikeButton count={props.count} />
+      {/* <LikeButton count={props.count} /> */}
     </div>
   );
 };
--- src/Blog.jsx
+++ src/Blog.jsx
@@ -3,63 +3,41 @@ import Article from './Article';
 import * as FooBar from './components/FooBar';
 import Hoge from './components/Hoge';
 
-class Blog extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isPublished: false,
-      count: 0,
-    };
-  }
+const Blog = () => {
+  // componentDidMount() {
+  //   // いいねをクリックされたら、数字がカウントアップする
+  //   document.getElementById('counter').addEventListener('click', this.countUp);
+  // }
 
-  componentDidMount() {
-    // いいねをクリックされたら、数字がカウントアップする
-    document.getElementById('counter').addEventListener('click', this.countUp);
-  }
+  // componentDidUpdate() {
+  //   // いいねを10回クリックしたら、数字が０に戻る
+  //   if (this.state.count >= 10) {
+  //     this.setState({ count: 0 });
+  //   }
+  // }
 
-  componentDidUpdate() {
-    // いいねを10回クリックしたら、数字が０に戻る
-    console.log(this.state.count);
-    if (this.state.count >= 10) {
-      this.setState({ count: 0 });
-    }
-  }
+  // componentWillUnmount() {
+  //   // コンポーネントが破棄される直前にリソースを開放するため、リスナーを解除する
+  //   document
+  //     .getElementById('counter')
+  //     .removeEventListener('click', this.countUp);
+  // }
 
-  componentWillUnmount() {
-    // コンポーネントが破棄される直前にリソースを開放するため、リスナーを解除する
-    document
-      .getElementById('counter')
-      .removeEventListener('click', this.countUp);
-  }
+  // countUp = () => {
+  //   this.setState({ count: this.state.count + 1 });
+  // };
 
-  //　公開状態を反転させる関数
-  togglePublished = () => {
-    this.setState({
-      isPublished: !this.state.isPublished,
-    });
-  };
-
-  countUp = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-
-  render() {
-    const authorName = 'Torahack';
-    return (
-      //　divダグで分けずに、コンポーネントを2つ以上書く場合は、React.Fragmentを使う　<> </>の省略形でも同じ
-      <>
-        <Article
-          title={'Reactの使い方'}
-          isPublished={this.state.isPublished}
-          toggle={() => this.togglePublished()}
-          count={this.state.count}
-        />
-        <FooBar.Foo />
-        <FooBar.Bar />
-        <Hoge />
-      </>
-    );
-  }
-}
-
+  return (
+    //　divダグで分けずに、コンポーネントを2つ以上書く場合は、React.Fragmentを使う　<> </>の省略形でも同じ
+    <>
+      <Article
+        title={'Reactの使い方'}
+        // count={this.state.count}
+      />
+      <FooBar.Foo />
+      <FooBar.Bar />
+      <Hoge />
+    </>
+  );
+};
 export default Blog; //クラスを宣言したらexportしておく
